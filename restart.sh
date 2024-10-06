174c0deed0ab88c7901dd2fe0f5ba8b59f152319
Tarek9100 <72744477+Tarek9100@users.noreply.github.com>
2024-10-06 17:41:22 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Stopping and removing container"
-docker stop flask-todo-app
-docker rm flask-todo-app
+sudo docker stop flask-todo-app
+sudo docker rm flask-todo-app
 
 echo "Starting a new container"
 
-docker run -d -p 5000:5000 --name flask-todo-app flask-todo-app
+sudo docker run -d -p 5000:5000 --name flask-todo-app flask-todo-app
 echo "Waiting for container to start..."
 sleep 5
