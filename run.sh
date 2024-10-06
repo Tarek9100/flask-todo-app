7b2db98f8a914f889bddfed1e892aafa63ac0cfe
Tarek9100 <72744477+Tarek9100@users.noreply.github.com>
2024-10-06 17:41:02 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Running docker container from flask-todo-app image"
-docker run -d -p 5000:5000 --name flask-todo-app flask-todo-app
+sudo docker run -d -p 5000:5000 --name flask-todo-app flask-todo-app
 
